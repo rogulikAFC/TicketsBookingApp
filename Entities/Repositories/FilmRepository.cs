8c8d457042449a41818e3d1bc688c9edf490358b
rogulik <malvla2008@gmail.com>
2024-05-08 19:50:06 +0300

    Film repository written.

---
+++ Entities/Repositories/FilmRepository.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace TicketsBookingApp.Entities.Repositories
+{
+    public class FilmRepository : IFilmRepository
+    {
+        private readonly TicketsBookingAppDbContext _dbContext;
+
+        public FilmRepository(TicketsBookingAppDbContext dbContext)
+        {
+            _dbContext = dbContext
+                ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public void Add(Film entity)
+        {
+            _dbContext.Films.Add(entity);
+        }
+
+        public void Delete(Film entity)
+        {
+            _dbContext.Films.Remove(entity);
+        }
+
+        public async Task<Film?> GetByIdAsync(int id)
+        {
+            return await _dbContext.Films
+                .FirstOrDefaultAsync(f => f.Id == id);
+        }
+
+        public async Task<IEnumerable<Film>> ListAsync(int pageNum, int pageSize)
+        {
+            return await _dbContext.Films
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+    }
+}
