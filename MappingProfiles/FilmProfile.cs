34cf3b913194c3033710906b5debae76ef8c88e8
rogulik <malvla2008@gmail.com>
2024-05-09 09:26:16 +0300

    Film DTOs and mapping profiles written.

---
+++ MappingProfiles/FilmProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TicketsBookingApp.Entities;
+using TicketsBookingApp.Models.Film;
+
+namespace TicketsBookingApp.MappingProfiles
+{
+    public class FilmProfile : Profile
+    {
+        public FilmProfile()
+        {
+            CreateMap<Film, FilmDto>()
+                .AfterMap((src, dest) => dest.AgeLimit = src.AgeLimit?.Value);
+
+            CreateMap<FilmForCreateDto, Film>();
+        }
+    }
+}
---
+++ Models/Film/FilmDto.cs
@@ -0,0 +1,15 @@
+﻿namespace TicketsBookingApp.Models.Film
+{
+    public class FilmDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string? Description { get; set; } = null!;
+
+        public int? AgeLimit { get; set; } 
+
+        public int? AgeOfRelease { get; set; }
+    }
+}
