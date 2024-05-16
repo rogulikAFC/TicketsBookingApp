b6d7a89864f6ba1cc3a7ecc9cbb5f0cfe1f23469
rogulik <malvla2008@gmail.com>
2024-05-16 18:43:07 +0300

    Tickets mapping profile written.

---
+++ MappingProfiles/TicketsProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TicketsBookingApp.Entities;
+using TicketsBookingApp.Models.Tickets;
+
+namespace TicketsBookingApp.MappingProfiles
+{
+    public class TicketsProfile : Profile
+    {
+        public TicketsProfile()
+        {
+            CreateMap<TicketForCreateDto, Ticket>();
+            
+            CreateMap<Ticket, TicketDto>();
+        }
+    }
+}
